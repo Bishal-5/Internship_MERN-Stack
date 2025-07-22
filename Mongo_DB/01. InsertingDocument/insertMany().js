2b5916f807db3f5aacd621dc86b60e74ff3dce5b
Bishal-5 <naskarbishal514@gmail.com>
2025-07-23 00:06:12 +0530

    MongoDB Complete

---
+++ Mongo_DB/01. InsertingDocument/insertMany().js
@@ -0,0 +1,81 @@
+db.books.insertMany([
+	{ _id: 5, title: 'SQl Query', price: "$25", author: "Sanadip Roy" },
+	{ _id: 3, title: 'NoSql Query', price: "$35", author: "Sanadip Roy" },
+	{ _id: 1, title: 'Express JS', price: "$25", author: "Sanadip Roy" },
+	{ _id: 2, title: 'React JS', price: "$35", author: "Sanadip Roy" },
+	{ _id: 6, title: 'Angular JS', price: "$25", author: "Sanadip Roy" },
+	{ _id: 4, title: 'Vanila JS', price: "$35", author: "Sanadip Roy" },
+	{ _id: 8, title: 'Mongo', price: "$25", author: "Sanadip Roy" },
+	{ _id: 7, title: 'Node', price: "$35", author: "Sanadip Roy" }
+], { ordered: false })
+
+db.books.insertMany([
+	{ _id: 5, title: 'SQl Query', price: "$25", author: "Sanadip Roy" },
+	{ _id: 3, title: 'NoSql Query', price: "$35", author: "Sanadip Roy" },
+	{ _id: 1, title: 'Express JS', price: "$25", author: "Sanadip Roy" },
+	{ _id: 2, title: 'React JS', price: "$35", author: "Sanadip Roy" },
+	{ _id: 6, title: 'Angular JS', price: "$25", author: "Sanadip Roy" },
+	{ _id: 4, title: 'Vanila JS', price: "$35", author: "Sanadip Roy" },
+	{ _id: 8, title: 'Mongo', price: "$25", author: "Sanadip Roy" },
+	{ _id: 7, title: 'Node', price: "$35", author: "Sanadip Roy" }
+], { ordered: true })
+
+db.products.insertMany([
+	{ "_id": 1, "name": "xPhone", "price": 799, "releaseDate": ISODate("2011-05-14"), "spec": { "ram": 4, "screen": 6.5, "cpu": 2.66 }, "color": ["white", "black"], "storage": [64, 128, 256] },
+	{ "_id": 2, "name": "xTablet", "price": 899, "releaseDate": ISODate("2011-09-01"), "spec": { "ram": 16, "screen": 9.5, "cpu": 3.66 }, "color": ["white", "black", "purple"], "storage": [128, 256, 512] },
+	{ "_id": 3, "name": "SmartTablet", "price": 899, "releaseDate": ISODate("2015-01-14"), "spec": { "ram": 12, "screen": 9.7, "cpu": 3.66 }, "color": ["blue"], "storage": [16, 64, 128] },
+	{ "_id": 4, "name": "SmartPad", "price": 699, "releaseDate": ISODate("2020-05-14"), "spec": { "ram": 8, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256, 1024] },
+	{ "_id": 5, "name": "SmartPhone", "price": 599, "releaseDate": ISODate("2022-09-14"), "spec": { "ram": 4, "screen": 5.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256] }
+])
+
+
+db.books.insertMany([
+	{ "_id": 1, "title": "Unlocking Android", "isbn": "1933988673", "categories": ["Open Source", "Mobile"] },
+	{ "_id": 2, "title": "Android in Action, Second Edition", "isbn": "1935182722", "categories": ["Java"] },
+	{ "_id": 3, "title": "Specification by Example", "isbn": "1617290084", "categories": ["Software Engineering"] },
+	{ "_id": 4, "title": "Flex 3 in Action", "isbn": "1933988746", "categories": ["Internet"] },
+	{ "_id": 5, "title": "Flex 4 in Action", "isbn": "1935182420", "categories": ["Internet"] },
+	{ "_id": 6, "title": "Collective Intelligence in Action", "isbn": "1933988312", "categories": ["Internet"] },
+	{ "_id": 7, "title": "Zend Framework in Action", "isbn": "1933988320", "categories": ["Web Development"] },
+	{ "_id": 8, "title": "Flex on Java", "isbn": "1933988797", "categories": ["Internet"] },
+	{ "_id": 9, "title": "Griffon in Action", "isbn": "1935182234", "categories": ["Java"] },
+	{ "_id": 10, "title": "OSGi in Depth", "isbn": "193518217X", "categories": ["Java"] },
+	{ "_id": 11, "title": "Flexible Rails", "isbn": "1933988509", "categories": ["Web Development"] },
+	{ "_id": 13, "title": "Hello! Flex 4", "isbn": "1933988762", "categories": ["Internet"] },
+	{ "_id": 14, "title": "Coffeehouse", "isbn": "1884777384", "categories": ["Miscellaneous"] },
+	{ "_id": 15, "title": "Team Foundation Server 2008 in Action", "isbn": "1933988592", "categories": ["Microsoft .NET"] },
+	{ "_id": 16, "title": "Brownfield Application Development in .NET", "isbn": "1933988711", "categories": ["Microsoft"] },
+	{ "_id": 17, "title": "MongoDB in Action", "isbn": "1935182870", "categories": ["Next Generation Databases"] },
+	{ "_id": 18, "title": "Distributed Application Development with PowerBuilder 6.0", "isbn": "1884777686", "categories": ["PowerBuilder"] },
+	{ "_id": 19, "title": "Jaguar Development with PowerBuilder 7", "isbn": "1884777864", "categories": ["PowerBuilder", "Client-Server"] },
+	{ "_id": 20, "title": "Taming Jaguar", "isbn": "1884777686", "categories": ["PowerBuilder"] }
+]);
+
+
+db.products.insertMany([
+	{ "_id": 1, "name": "xPhone", "price": 799, "releaseDate": ISODate("2011-05-14T00:00:00Z"), "spec": { "ram": 4, "screen": 6.5, "cpu": 2.66 }, "color": ["white", "black"], "storage": [64, 128, 256] },
+	{ "_id": 2, "name": "xTablet", "price": 899, "releaseDate": ISODate("2011-09-01T00:00:00Z"), "spec": { "ram": 16, "screen": 9.5, "cpu": 3.66 }, "color": ["white", "black", "purple"], "storage": [128, 256, 512] },
+	{ "_id": 3, "name": "SmartTablet", "price": 899, "releaseDate": ISODate("2015-01-14T00:00:00Z"), "spec": { "ram": 12, "screen": 9.7, "cpu": 3.66 }, "color": ["blue"], "storage": [16, 64, 128] },
+	{ "_id": 4, "name": "SmartPad", "price": 699, "releaseDate": ISODate("2020-05-14T00:00:00Z"), "spec": { "ram": 8, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256, 1024] },
+	{ "_id": 5, "name": "SmartPhone", "price": 599, "releaseDate": ISODate("2022-09-14T00:00:00Z"), "spec": { "ram": 4, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256] },
+	{ "_id": 6, "name": "xWidget", "spec": { "ram": 64, "screen": 9.7, "cpu": 3.66 }, "color": ["black"], "storage": [1024] },
+	{ "_id": 7, "name": "xReader", "price": null, "spec": { "ram": 64, "screen": 6.7, "cpu": 3.66 }, "color": ["black", "white"], "storage": [128] }
+])
+
+db.products.insertMany([
+	{ "_id": 1, "name": "xPhone", "price": "799", "releaseDate": ISODate("2011-05-14T00:00:00Z"), "spec": { "ram": 4, "screen": 6.5, "cpu": 2.66 }, "color": ["white", "black"], "storage": [64, 128, 256] },
+	{ "_id": 2, "name": "xTablet", "price": NumberInt(899), "releaseDate": ISODate("2011-09-01T00:00:00Z"), "spec": { "ram": 16, "screen": 9.5, "cpu": 3.66 }, "color": ["white", "black", "purple"], "storage": [128, 256, 512] },
+	{ "_id": 3, "name": "SmartTablet", "price": NumberInt(899), "releaseDate": ISODate("2015-01-14T00:00:00Z"), "spec": { "ram": 12, "screen": 9.7, "cpu": 3.66 }, "color": ["blue"], "storage": [16, 64, 128] },
+	{ "_id": 4, "name": "SmartPad", "price": [599, 699, 799], "releaseDate": ISODate("2020-05-14T00:00:00Z"), "spec": { "ram": 8, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256, 1024] },
+	{ "_id": 5, "name": "SmartPhone", "price": ["599", 699], "releaseDate": ISODate("2022-09-14T00:00:00Z"), "spec": { "ram": 4, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256] },
+	{ "_id": 6, "name": "xWidget", "spec": { "ram": 64, "screen": 9.7, "cpu": 3.66 }, "color": ["black"], "storage": [1024] }
+])
+
+db.products.insertMany([
+	{ "_id": 1, "name": "xPhone", "price": 799, "releaseDate": ISODate("2011-05-14T00:00:00Z"), "spec": { "ram": 4, "screen": 6.5, "cpu": 2.66 }, "color": ["white", "black"], "storage": [64, 128, 256] },
+	{ "_id": 2, "name": "xTablet", "price": 899, "releaseDate": ISODate("2011-09-01T00:00:00Z"), "spec": { "ram": 16, "screen": 9.5, "cpu": 3.66 }, "color": ["white", "black", "purple"], "storage": [128, 256, 512] },
+	{ "_id": 3, "name": "SmartTablet", "price": 899, "releaseDate": ISODate("2015-01-14T00:00:00Z"), "spec": { "ram": 12, "screen": 9.7, "cpu": 3.66 }, "color": ["blue"], "storage": [16, 64, 128] },
+	{ "_id": 4, "name": "SmartPad", "price": 699, "releaseDate": ISODate("2020-05-14T00:00:00Z"), "spec": { "ram": 8, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256, 1024] },
+	{ "_id": 5, "name": "SmartPhone", "price": 599, "releaseDate": ISODate("2022-09-14T00:00:00Z"), "spec": { "ram": 4, "screen": 9.7, "cpu": 1.66 }, "color": ["white", "orange", "gold", "gray"], "storage": [128, 256] },
+	{ "_id": 6, "name": "xWidget", "spec": { "ram": 64, "screen": 9.7, "cpu": 3.66 }, "color": ["black"], "storage": [1024] }
+])
---
+++ Mongo_DB/06.ArrayQueryOperator/$size.js
@@ -0,0 +1,6 @@
+db.products.find({})
+
+db.products.find({ color: { $size: 2 } })
+
+db.products.find({ $or: [{ color: { $size: 1 } }, { color: { $size: 2 } }] })
+db.products.find({ $nor: [{ color: { $size: 1 } }, { color: { $size: 2 } }] })
