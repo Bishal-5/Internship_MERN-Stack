673755985074dbe5ab3d097c486c48671ac0423e
Bishal-5 <naskarbishal514@gmail.com>
2025-07-22 15:48:04 +0530

    React

--- Frontends/02.components/src/App.jsx
+++ Frontends/02.components/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import BasicFunctionalComponent from "./components/BasicFunctionalComponent";
+import ComplexFunctionalComponent from './components/ComplexFunctionalComponent';
+import FunctionalComponentLifecycle from './components/FunctionalComponentLifecycle';
 
 const App = () => {
   return (
     <>
       <h1>App Work</h1>
       <BasicFunctionalComponent />
+      <ComplexFunctionalComponent />
+      <FunctionalComponentLifecycle />
     </>
   )
 }
