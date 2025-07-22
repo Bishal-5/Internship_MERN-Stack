0c907c7d2539a8c30e5800b43ee06420883deaa4
Bishal-5 <naskarbishal514@gmail.com>
2025-07-22 13:19:39 +0530

    React

---
+++ Frontends/02.components/src/pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div className='page services-page'>
+            <h1>Our Services</h1>
+        </div>
+    )
+}
+
+export default Services
